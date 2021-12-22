f7fc3c934b93a7baf73d8427a5b045cb0ca05e54
Sergey Kuznetsov <mr.enline@gmail.com>
2021-12-22 18:54:20 +0300

    Исправлен баг с recyclerView путем добавления условия в метод onBindViewHolder и переопределения метода onViewRecycled, который отвечает за переиспользование ViewHolder при прокрутке

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -8,21 +8,28 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var ll_shop_list: LinearLayout
+    private lateinit var adapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ll_shop_list = findViewById(R.id.ll_shop_list)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-
+            adapter.shopItemList = it
         }
+    }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list);
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
@@ -12,7 +13,7 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
     var shopItemList = listOf<ShopItem>()
         set(value) {
-            shopItemList = value
+            field = value
             notifyDataSetChanged()
         }
 
@@ -22,12 +23,30 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        holder.tvName.text = shopItemList[position].name
-        holder.tvCount.text = shopItemList[position].count.toString()
+        val shopItem = shopItemList[position]
+        val status = if (shopItem.enabled) {
+            "Активен"
+        } else {
+            "Не активен"
+        }
+        holder.view.setOnLongClickListener { true }
+
+        if (shopItem.enabled) {
+            holder.tvName.text = "${shopItem.name} $status"
+            holder.tvCount.text = "${shopItem.count}"
+            holder.tvName.setTextColor(ContextCompat.getColor(holder.view.context, android.R.color.holo_red_dark))
+        }
     }
 
     override fun getItemCount(): Int {
         return shopItemList.size
+    }
+
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.tvName.text = ""
+        holder.tvCount.text = ""
+        holder.tvName.setTextColor(ContextCompat.getColor(holder.view.context, android.R.color.white))
     }
 
     class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder (view) {
